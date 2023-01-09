5e3a0ea0d162c020321c7a4a673ce8954a19874a
Dino Costantini <dino.costantini@gmail.com>
2023-01-09 13:52:37 +0000

    Day12: more efficient and cross platform!

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -2,56 +2,84 @@ use std::collections::HashSet;
 
 pub fn day12() {
     let map = include_bytes!("../../day12.txt");
-    let start = map.iter().position(|&x| x == b'S').unwrap();
-    let end = map.iter().position(|&x| x == b'E').unwrap();
 
-    let result1 = spf(&map, start, b'E', |s, c| s <= c + 1);
-    let result2 = spf(&map, end, b'a', |s, c| s >= c - 1);
+    let (start, end, line_len, terminator_len) = match (
+        map.iter().position(|&x| x == b'S'),
+        map.iter().position(|&x| x == b'E'),
+        map.iter().position(|&x| x == 10),
+        map.iter().position(|&x| x == 13),
+    ) {
+        (Some(s), Some(e), Some(t), None) | (Some(s), Some(e), None, Some(t)) => (s, e, t + 1, 1),
+        (Some(s), Some(e), Some(t), Some(n)) => (s, e, t.min(n) + 2, 2),
+        _ => unreachable!(),
+    };
+
+    let result1 = spf(
+        &map,
+        start,
+        |map, ix| neighborhood(map, ix, line_len, terminator_len, |s, c| s <= c + 1),
+        |(ix, _)| ix == end,
+    );
+    let result2 = spf(
+        &map,
+        end,
+        |map, ix| neighborhood(map, ix, line_len, terminator_len, |s, c| s >= c - 1),
+        |(_, p)| p == b'a',
+    );
 
     println!("DAY 12\nSolution 1: {result1}\nSolution 2: {result2}");
 }
 
-fn spf<const L: usize, F>(
+fn spf<const L: usize, NF, EF>(
     map: &[u8; L],
     start_position: usize,
-    end: u8,
-    is_valid_step: F
+    neighbors: NF,
+    exit_condition: EF,
 ) -> usize
 where
-F: Fn(u8, u8) -> bool
-    {
-    match (1..).try_fold(
+    NF: Fn(&[u8; L], usize) -> Vec<(usize, u8)>,
+    EF: Fn((usize, u8)) -> bool,
+{
+    match (1usize..).try_fold(
         (
             HashSet::from([start_position]),
             HashSet::from([start_position]),
-            1,
         ),
-        |(frontier, mut visited, result), ix| {
-            if frontier.iter().any(|&ix| map[ix] == end) {
-                Err((frontier, visited, result))
-            } else {
-                visited.extend(frontier.iter());
+        |(frontier, mut visited), ix| {
+            visited.extend(frontier.iter());
 
-                Ok((
-                    frontier
-                        .iter()
-                        .flat_map(|&ix| neighborhood(map, ix, &is_valid_step))
-                        .filter(|v| !visited.contains(v))
-                        .collect(),
-                    visited,
-                    ix,
-                ))
-            }
+            Ok((
+                frontier
+                    .iter()
+                    .flat_map(|&ix| neighbors(map, ix)) //neighborhood(map, ix, &is_valid_step))
+                    .filter(|(ix, _)| !visited.contains(ix))
+                    .try_fold(HashSet::new(), |mut acc, curr| {
+                        if exit_condition(curr) {
+                            Err(ix)
+                        } else {
+                            acc.insert(curr.0);
+                            Ok(acc)
+                        }
+                    })?,
+                visited,
+            ))
         },
     ) {
         Ok(_) => unreachable!(),
-        Err((_, _, result)) => result,
+        Err(result) => result,
     }
 }
 
-fn neighborhood<const L: usize, F>(map: &[u8; L], ix: usize, is_valid_step: F)  -> Vec<usize>
+fn neighborhood<const L: usize, F>(
+    map: &[u8; L],
+    ix: usize,
+    line_len: usize,
+    terminator_len: usize,
+    is_valid_step: F,
+) -> Vec<(usize, u8)>
 where
-    F: Fn(u8, u8) -> bool {
+    F: Fn(u8, u8) -> bool,
+{
     let mut n = vec![];
     let c = match map[ix] {
         b'S' => b'a',
@@ -59,20 +87,20 @@ where
         c => c,
     };
 
-    if ix % 183 > 0 && is_valid_step(map[ix - 1], c) {
-        n.push(ix - 1);
+    if ix % line_len > 0 && is_valid_step(map[ix - 1], c) {
+        n.push((ix - 1, map[ix - 1]));
     }
 
-    if ix % 183 < 180 && is_valid_step(map[ix + 1], c) {
-        n.push(ix + 1)
+    if ix % line_len < (line_len - terminator_len - 1) && is_valid_step(map[ix + 1], c) {
+        n.push((ix + 1, map[ix + 1]));
     }
 
-    if ix > 183 && is_valid_step(map[ix - 183], c) {
-        n.push(ix - 183)
+    if ix > line_len && is_valid_step(map[ix - line_len], c) {
+        n.push((ix - line_len, map[ix - line_len]));
     }
 
-    if ix < L - 184 && is_valid_step(map[ix + 183], c) {
-        n.push(ix + 183)
+    if ix < L - (line_len + 1) && is_valid_step(map[ix + line_len], c) {
+        n.push((ix + line_len, map[ix + line_len]));
     }
 
     n
