4a00bbdb9ca0dcb45b06b21389a83879abef8ad6
Dino Costantini <dino.costantini@gmail.com>
2023-01-09 11:08:08 +0000

    Day12: refacto

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -5,8 +5,8 @@ pub fn day12() {
     let start = map.iter().position(|&x| x == b'S').unwrap();
     let end = map.iter().position(|&x| x == b'E').unwrap();
 
-    let result1 = spf(&map, start, b'E', neighborhood);
-    let result2 = spf(&map, end, b'a', reverse_neighborhood);
+    let result1 = spf(&map, start, b'E', |m, ix| neighborhood(m, ix, |s, c| s <= c + 1));
+    let result2 = spf(&map, end, b'a', |m, ix| neighborhood(m, ix, |s, c| s >= c - 1));
 
     println!("DAY 12\nSolution 1: {result1}\nSolution 2: {result2}");
 }
@@ -26,9 +26,9 @@ where
             HashSet::from([start_position]),
             1,
         ),
-        |(frontier, mut visited, _), ix| {
+        |(frontier, mut visited, result), ix| {
             if frontier.iter().any(|&ix| map[ix] == end) {
-                Err((frontier, visited, ix - 1))
+                Err((frontier, visited, result))
             } else {
                 visited.extend(frontier.iter());
 
@@ -49,7 +49,9 @@ where
     }
 }
 
-fn reverse_neighborhood<const L: usize>(map: &[u8; L], ix: usize) -> Vec<usize> {
+fn neighborhood<const L: usize, F>(map: &[u8; L], ix: usize, is_valid_step: F)  -> Vec<usize>
+where
+    F: Fn(u8, u8) -> bool {
     let mut n = vec![];
     let c = match map[ix] {
         b'S' => b'a',
@@ -57,46 +59,19 @@ fn reverse_neighborhood<const L: usize>(map: &[u8; L], ix: usize) -> Vec<usize> 
         c => c,
     };
 
-    if ix % 183 > 0 && map[ix - 1] >= c - 1 {
+    if ix % 183 > 0 && is_valid_step(map[ix - 1], c) {
         n.push(ix - 1);
     }
 
-    if ix % 183 < 180 && map[ix + 1] >= c - 1 {
+    if ix % 183 < 180 && is_valid_step(map[ix + 1], c) {
         n.push(ix + 1)
     }
 
-    if ix > 183 && map[ix - 183] >= c - 1 {
+    if ix > 183 && is_valid_step(map[ix - 183], c) {
         n.push(ix - 183)
     }
 
-    if ix < L - 184 && map[ix + 183] >= c - 1 {
-        n.push(ix + 183)
-    }
-
-    n
-}
-
-fn neighborhood<const L: usize>(map: &[u8; L], ix: usize) -> Vec<usize> {
-    let mut n = vec![];
-    let c = match map[ix] {
-        b'S' => b'a',
-        b'E' => b'z',
-        c => c,
-    };
-
-    if ix % 183 > 0 && map[ix - 1] <= c + 1 {
-        n.push(ix - 1);
-    }
-
-    if ix % 183 < 180 && map[ix + 1] <= c + 1 {
-        n.push(ix + 1)
-    }
-
-    if ix > 183 && map[ix - 183] <= c + 1 {
-        n.push(ix - 183)
-    }
-
-    if ix < L - 184 && map[ix + 183] <= c + 1 {
+    if ix < L - 184 && is_valid_step(map[ix + 183], c) {
         n.push(ix + 183)
     }
 
