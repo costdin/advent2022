5c95af5d4c5d01afc8c7b65eb4fb8f56feec4b84
Dino Costantini <dino.costantini@gmail.com>
2023-01-26 16:28:39 +0000

    day16: faster!

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -1,18 +1,27 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    ops::Index,
+};
 
 use itertools::Itertools;
 
+fn to_id(s: &str) -> u16 {
+    s.chars()
+        .map(|c| c as u16 - 'A' as u16)
+        .fold(0, |acc, c| (acc << 8) + c)
+}
+
 pub fn day16() {
-    let (press, all) = include_str!("../../day16.txt")
+    let (mut press, all) = include_str!("../../day16.txt")
         .trim()
         .lines()
         .map(|l| l.split_once('=').unwrap())
         .map(|(s1, s2)| (&s1[6..8], s2.split_once(';').unwrap()))
         .map(|(id, (s1, s2))| {
             (
-                id,
+                to_id(id),
                 s1.parse::<u32>().unwrap(),
-                s2[23..].trim().split(", ").collect_vec(),
+                s2[23..].trim().split(", ").map(to_id).collect_vec(),
             )
         })
         .fold(
@@ -27,34 +36,41 @@ pub fn day16() {
             },
         );
 
-    let map = ["AA"]
+    let map = [0]
         .iter()
         .chain(press.iter())
-        .map(|p| (*p, spf(p, &press, &all)))
+        .map(|p| (*p, spf(*p, &press, &all)))
         .collect::<HashMap<_, _>>();
 
-    let result1 = gaga(&press, 0, &map["AA"], &map);
-    let result2 = gaga2(&press, 0, 0, &map["AA"], &map["AA"], &map);
+    let (first_step, result1) = gaga(&press, 0, &map[&0], &map);
+    let xx = &map[&first_step];
+    let yy = &map[&0][&first_step];
+
+    press.retain(|v| v != &first_step);
+    let result2 = gaga2(&press, yy.0 + 1, 0, xx, &map[&0], &map) + (25 - yy.0) * yy.1;
 
     println!("DAY 16\nSolution 1: {result1}\nSolution 2: {result2}");
 }
 
 fn gaga<'a>(
-    nodes: &'a Vec<&'a str>,
+    nodes: &'a Vec<u16>,
     distance: u32,
-    next: &'a HashMap<&str, (u32, u32)>,
-    map: &'a HashMap<&str, HashMap<&str, (u32, u32)>>,
-) -> u32 {
+    next: &'a HashMap<u16, (u32, u32)>,
+    map: &'a HashMap<u16, HashMap<u16, (u32, u32)>>,
+) -> (u16, u32) {
     let mut n = nodes.clone();
-    let mut result = 0;
+    let mut result = (0, 0);
 
     for i in 0..n.len() {
         let e = n.remove(i);
 
-        let new_distance = distance + next[e].0 + 1;
+        let new_distance = distance + next[&e].0 + 1;
         if new_distance < 30 {
-            let max = gaga(&n, new_distance, &map[e], map);
-            result = result.max(max + (30 - new_distance) * next[e].1);
+            let max = gaga(&n, new_distance, &map[&e], map).1 + (30 - new_distance) * next[&e].1;
+
+            if max > result.1 {
+                result = (e, result.1.max(max));
+            }
         }
 
         n.insert(i, e);
@@ -64,18 +80,18 @@ fn gaga<'a>(
 }
 
 fn gaga2<'a>(
-    nodes: &'a Vec<&'a str>,
+    nodes: &'a Vec<u16>,
     distance: u32,
     distance2: u32,
-    next: &'a HashMap<&str, (u32, u32)>,
-    next2: &'a HashMap<&str, (u32, u32)>,
-    map: &'a HashMap<&str, HashMap<&str, (u32, u32)>>,
+    next: &'a HashMap<u16, (u32, u32)>,
+    next2: &'a HashMap<u16, (u32, u32)>,
+    map: &'a HashMap<u16, HashMap<u16, (u32, u32)>>,
 ) -> u32 {
     let mut n = nodes.clone();
     let mut result = 0;
 
     for i in 0..n.len() {
-        let e = n.remove(i);
+        let e = &n.remove(i);
 
         let (new_distance, n1, new_distance2, n2, nd) = if distance <= distance2 {
             (
@@ -97,21 +113,21 @@ fn gaga2<'a>(
 
         if new_distance < 26 && new_distance2 < 26 {
             let max = gaga2(&n, new_distance, new_distance2, n1, n2, map);
-            
+
             result = result.max(max + (26 - nd) * next[e].1);
         }
 
-        n.insert(i, e);
+        n.insert(i, *e);
     }
 
     result
 }
 
 fn spf<'a>(
-    start: &'a str,
-    destinations: &[&str],
-    table: &'a HashMap<&'a str, (u32, Vec<&'a str>)>,
-) -> HashMap<&'a str, (u32, u32)> {
+    start: u16,
+    destinations: &[u16],
+    table: &'a HashMap<u16, (u32, Vec<u16>)>,
+) -> HashMap<u16, (u32, u32)> {
     let mut res = HashMap::new();
     let mut frontier = vec![start];
     let mut ix = 0;
@@ -121,11 +137,11 @@ fn spf<'a>(
         let mut new_frontier = HashSet::new();
 
         while let Some(e) = frontier.pop() {
-            if !res.contains_key(e) && destinations.contains(&e) && e != start {
-                res.insert(e, (ix, table[e].0));
+            if !res.contains_key(&e) && destinations.contains(&e) && e != start {
+                res.insert(e, (ix, table[&e].0));
             }
 
-            new_frontier.extend(&table[e].1);
+            new_frontier.extend(&table[&e].1);
         }
 
         frontier = new_frontier.into_iter().collect();
